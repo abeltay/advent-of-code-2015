617ffe89d2374d60d38b79cd0d3b86d0eb08eb8d
Abel Tay <15604207+abeltay@users.noreply.github.com>
2023-01-24 21:52:35 +0800

    Use a script to pull the day's input.txt

--- input.sh
+++ input.sh
@@ -1,2 +1,9 @@
-value=`cat day.txt`
-mv ~/Downloads/input.txt $value/
+#!/bin/sh
+
+day=`cat day.txt`
+url_day=$(echo $day | sed 's/^0*//')
+cookie=`cat cookie.txt`
+
+curl --location --request GET "https://adventofcode.com/2015/day/$url_day/input" \
+	--header "Cookie: $cookie" --output "$day/input.txt"
+
