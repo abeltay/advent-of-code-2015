93999328aee528a73b9d14a97402f0fd6090f9fd
Abel Tay <15604207+abeltay@users.noreply.github.com>
2023-01-24 14:10:16 +0800

    Add template

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 My attempt at <https://adventofcode.com/2015>
 
 > Note: All task input files are excluded from the repository with `.gitignore` – we should not post them publicly, as Eric Wastl asks for: [Tweet](https://twitter.com/ericwastl/status/1465805354214830081).
+
+# To run the scripts
+1. Run `./prep.sh 01` with the day number appended. The example here is for Day 01
+2. Run `./input.sh` to copy the input file from Downloads folder
+3. Run `./run.sh` to run current day's
+
+## To test the template
+1. Set day.txt to `template`
+2. Uncomment the code block for part2
+3. Run `./run.sh`
---
+++ template/main.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+fn parse_input(filename: &str) -> Vec<(i64, i64, i64)> {
+    let contents = fs::read_to_string(filename).expect("File not found");
+    let mut lines: Vec<(i64, i64, i64)> = Vec::new();
+    for line in contents.trim().split('\n') {
+        let mut split = line.split('x');
+        let dimension: (i64, i64, i64) = (
+            split.next().expect("must be int").parse::<i64>().unwrap(),
+            split.next().expect("must be int").parse::<i64>().unwrap(),
+            split.next().expect("must be int").parse::<i64>().unwrap());
+        lines.push(dimension)
+    }
+    return lines.to_vec();
+}
+
+fn part1(filename: &str) -> i64 {
+    let input = parse_input(filename);
+    println!("{:?}", input);
+    return 0;
+}
+
+fn _part2(filename: &str) -> i64 {
+    println!("{}", filename);
+    return 0;
+}
+
+fn main() {
+    let filename = "input_test.txt";
+    let answer = part1(filename);
+    assert_eq!(answer, 0);
+    // let answer = part2(filename);
+    // assert_eq!(answer, 0);
+
+    println!("Test passed, running input");
+    let filename = "input.txt";
+    println!("Part 1: {}", part1(filename));
+    // println!("Part 2: {}", part2(filename));
+}
